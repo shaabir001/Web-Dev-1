546c6292c84bad7e28600404f59274de63c0776d
Jacob-S15 <305856@shakopeeschools.org>
2025-01-07 09:51:45 -0600

    games.html updated

--- Version7.0/user15/JS/script.js
+++ Version7.0/user15/JS/script.js
@@ -1,200 +1,91 @@
-import { WORDS } from "./words.js";
+const typingText = document.querySelector(".typing-text p"),
+inpField = document.querySelector(".wrapper .input-field"),
+tryAgainBtn = document.querySelector(".content button"),
+timeTag = document.querySelector(".time span b"),
+mistakeTag = document.querySelector(".mistake span"),
+wpmTag = document.querySelector(".wpm span"),
+cpmTag = document.querySelector(".cpm span");
 
-const NUMBER_OF_GUESSES = 6;
-let guessesRemaining = NUMBER_OF_GUESSES;
-let currentGuess = [];
-let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+let timer,
+maxTime = 60,
+timeLeft = maxTime,
+charIndex = mistakes = isTyping = 0;
 
-console.log(rightGuessString);
-
-function initBoard() {
-  let board = document.getElementById("game-board");
-
-  for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
-    let row = document.createElement("div");
-    row.className = "letter-row";
-
-    for (let j = 0; j < 5; j++) {
-      let box = document.createElement("div");
-      box.className = "letter-box";
-      row.appendChild(box);
-    }
-
-    board.appendChild(row);
-  }
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    paragraphs[ranIndex].split("").forEach(char => {
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
 }
 
-function shadeKeyBoard(letter, color) {
-  for (const elem of document.getElementsByClassName("keyboard-button")) {
-    if (elem.textContent === letter) {
-      let oldColor = elem.style.backgroundColor;
-      if (oldColor === "green") {
-        return;
-      }
+function initTyping() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if(charIndex < characters.length - 1 && timeLeft > 0) {
+        if(!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if(typedChar == null) {
+            if(charIndex > 0) {
+                charIndex--;
+                if(characters[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if(characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
 
-      if (oldColor === "yellow" && color !== "green") {
-        return;
-      }
-
-      elem.style.backgroundColor = color;
-      break;
-    }
-  }
+        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        
+        wpmTag.innerText = wpm;
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        clearInterval(timer);
+        inpField.value = "";
+    }   
 }
 
-function deleteLetter() {
-  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
-  let box = row.children[nextLetter - 1];
-  box.textContent = "";
-  box.classList.remove("filled-box");
-  currentGuess.pop();
-  nextLetter -= 1;
+function initTimer() {
+    if(timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
 }
 
-function checkGuess() {
-  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
-  let guessString = "";
-  let rightGuess = Array.from(rightGuessString);
-
-  for (const val of currentGuess) {
-    guessString += val;
-  }
-
-  if (guessString.length != 5) {
-    toastr.error("Not enough letters!");
-    return;
-  }
-
-  if (!WORDS.includes(guessString)) {
-    toastr.error("Word not in list!");
-    return;
-  }
-
-  var letterColor = ["gray", "gray", "gray", "gray", "gray"];
-
-  //check green
-  for (let i = 0; i < 5; i++) {
-    if (rightGuess[i] == currentGuess[i]) {
-      letterColor[i] = "green";
-      rightGuess[i] = "#";
-    }
-  }
-
-  //check yellow
-  //checking guess letters
-  for (let i = 0; i < 5; i++) {
-    if (letterColor[i] == "green") continue;
-
-    //checking right letters
-    for (let j = 0; j < 5; j++) {
-      if (rightGuess[j] == currentGuess[i]) {
-        letterColor[i] = "yellow";
-        rightGuess[j] = "#";
-      }
-    }
-  }
-
-  for (let i = 0; i < 5; i++) {
-    let box = row.children[i];
-    let delay = 250 * i;
-    setTimeout(() => {
-      //flip box
-      animateCSS(box, "flipInX");
-      //shade box
-      box.style.backgroundColor = letterColor[i];
-      shadeKeyBoard(guessString.charAt(i) + "", letterColor[i]);
-    }, delay);
-  }
-
-  if (guessString === rightGuessString) {
-    toastr.success("You guessed right! Game over!");
-    guessesRemaining = 0;
-    return;
-  } else {
-    guessesRemaining -= 1;
-    currentGuess = [];
-    nextLetter = 0;
-
-    if (guessesRemaining === 0) {
-      toastr.error("You've run out of guesses! Game over!");
-      toastr.info(`The right word was: "${rightGuessString}"`);
-    }
-  }
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
 }
 
-function insertLetter(pressedKey) {
-  if (nextLetter === 5) {
-    return;
-  }
-  pressedKey = pressedKey.toLowerCase();
-
-  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
-  let box = row.children[nextLetter];
-  animateCSS(box, "pulse");
-  box.textContent = pressedKey;
-  box.classList.add("filled-box");
-  currentGuess.push(pressedKey);
-  nextLetter += 1;
-}
-
-const animateCSS = (element, animation, prefix = "animate__") =>
-  // We create a Promise and return it
-  new Promise((resolve, reject) => {
-    const animationName = `${prefix}${animation}`;
-    // const node = document.querySelector(element);
-    const node = element;
-    node.style.setProperty("--animate-duration", "0.3s");
-
-    node.classList.add(`${prefix}animated`, animationName);
-
-    // When the animation ends, we clean the classes and resolve the Promise
-    function handleAnimationEnd(event) {
-      event.stopPropagation();
-      node.classList.remove(`${prefix}animated`, animationName);
-      resolve("Animation ended");
-    }
-
-    node.addEventListener("animationend", handleAnimationEnd, { once: true });
-  });
-
-document.addEventListener("keyup", (e) => {
-  if (guessesRemaining === 0) {
-    return;
-  }
-
-  let pressedKey = String(e.key);
-  if (pressedKey === "Backspace" && nextLetter !== 0) {
-    deleteLetter();
-    return;
-  }
-
-  if (pressedKey === "Enter") {
-    checkGuess();
-    return;
-  }
-
-  let found = pressedKey.match(/[a-z]/gi);
-  if (!found || found.length > 1) {
-    return;
-  } else {
-    insertLetter(pressedKey);
-  }
-});
-
-document.getElementById("keyboard-cont").addEventListener("click", (e) => {
-  const target = e.target;
-
-  if (!target.classList.contains("keyboard-button")) {
-    return;
-  }
-  let key = target.textContent;
-
-  if (key === "Del") {
-    key = "Backspace";
-  }
-
-  document.dispatchEvent(new KeyboardEvent("keyup", { key: key }));
-});
-
-initBoard();
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
