f1e10fbab98727ac722685995656d143ed251aec
Seihx <230345@shakopeeschools.org>
2025-01-09 08:47:55 -0600

    Added Three Games, changed RPS and TTT

---
+++ Version7.0/user14/JS/script.js
@@ -0,0 +1,10 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+}
+
+
+function changeText() {
+    document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+    document.getElementById("textChange").style
+
+}
